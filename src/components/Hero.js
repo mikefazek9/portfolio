66cb4c962fa7a56d97841eef29e313bfb2343d67
unknown <mike.fazek@gmail.com>
2020-11-27 05:08:01 -0600

    change hero btn colors- in process of dropdown menu

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,12 +1,19 @@
 import React from "react";
 
 const Hero = (props) => {
+  const social = {
+    twitter: "https://twitter.com/mike_fazek",
+    facebook: "https://www.facebook.com/Michael.Fazekas34/",
+    gmail: "mike.fazek@gmail.com",
+    linkedIn: "https://www.linkedin.com/in/michael-fazekas-10267b101/",
+    github: "https://github.com/mikefazek9",
+  };
   return (
     <div className="hero_section">
       <h2 className="hero_title">{props.title}...</h2>
       <div className="links">
-        <button className="btn-github">{props.github}</button>
-        <button className="btn-linkedin">{props.linkedIn}</button>
+        <button className="btn-github"><a href={social.github} className="btn-hero">{props.github}</a></button>
+        <button className="btn-linkedin"><a href={social.linkedIn} className="btn-hero">{props.linkedIn}</a></button>
       </div>
     </div>
   );
